d7032014bc747edf57531650fac0f5bb2d4fe445
Uriel8334 <uaandrade@espe.edu.ec>
2026-01-22 10:31:32 -0500

    Agregada prueba con JUnit, con nombre UserExistTest()

--- src/test/java/es/upm/grise/profundizacion/subscriptionService/SubscriptionServiceTest.java
+++ src/test/java/es/upm/grise/profundizacion/subscriptionService/SubscriptionServiceTest.java
@@ -1,10 +1,21 @@
 package es.upm.grise.profundizacion.subscriptionService;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+
 
 public class SubscriptionServiceTest {
 	
 	@Test
 	public void smokeTest() {}
 	
+	@Test( )
+	public void UserExistTest(){
+		User user = new User();
+		SubscriptionService subUser = new SubscriptionService();
+		user.setDelivery(Delivery.DO_NOT_DELIVER);
+		user.setEmail("jorge@gmail.com");
+		assertEquals(user, subUser.getSubscribers());
+	}
 }
